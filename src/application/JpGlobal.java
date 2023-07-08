5f01ea7cbe71e1c1edd0bc0a6040e30fc5da9cec
Kelly Wiles <rkwiles@twc.com>
2023-07-08 13:16:11 -0500

    Added linux jardesc.

--- src/application/JpGlobal.java
+++ src/application/JpGlobal.java
@@ -47,7 +47,7 @@ public class JpGlobal {
 	}
 	
 	private void initGlobals() {
-		appVersion = "1.1.12";
+		appVersion = "1.1.13";
 		
 		String os = System.getProperty("os.name").toLowerCase();
 		if (os.contains("win") == true) {
